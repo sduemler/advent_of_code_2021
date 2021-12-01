1732f1364c5030ec09b02405230a7b0a78517f1b
sduemelr <samduemler@gmail.com>
2021-12-01 11:24:40 -0600

    Solved day 1 part 2

--- 1/solution1.py
+++ 1/solution1.py
@@ -1,13 +1,32 @@
 input = open("input1.txt", "r")
 lines = input.readlines()
-increase = 0
-previous = int(str.rstrip(lines[0]))
-lines = lines[1:]
 
+# Helper
+
+
+def stripper(n):
+    return int(str.rstrip(n))
+
+
+lines = list(map(stripper, lines))
+
+# Part 1
+increase = 0
+previous = lines[0]
 for line in lines:
-    num = int(str.rstrip(line))
+    num = line
     if num > previous:
         increase += 1
     previous = num
 
 print(increase)
+
+# Part 2
+windowIncrease = 0
+window1 = lines[0] + lines[1] + lines[2]
+for x in range(3, len(lines)):
+    window2 = window1 - lines[x-3] + lines[x]
+    if window2 > window1:
+        windowIncrease += 1
+    window1 = window2
+print(windowIncrease)
