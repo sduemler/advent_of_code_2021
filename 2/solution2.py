2f398fe5f2f0439899e87c213ffcb035fabb9bbe
sduemelr <samduemler@gmail.com>
2021-12-02 13:39:57 -0600

    Solved day 2 part 1

---
+++ 2/solution2.py
@@ -0,0 +1,27 @@
+input = open("input2.txt", "r")
+lines = input.readlines()
+directions = []
+x = 0
+y = 0
+
+
+def formatter(n):
+    line = str.rstrip(n)
+    dir = line.split(" ")
+    dir[1] = int(dir[1])
+    directions.append(dir)
+
+
+output = list(map(formatter, lines))
+
+for i in range(len(directions)):
+    dir = directions[i][0]
+    dist = directions[i][1]
+    if dir == 'forward':
+        x += dist
+    elif dir == 'down':
+        y += dist
+    elif dir == 'up':
+        y -= dist
+
+print(x * y)
