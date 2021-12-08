65c7c3001c27c263672af2f46428f6c1208fcf3f
sduemelr <samduemler@gmail.com>
2021-12-07 19:25:48 -0600

    Solved day 4 part 1

--- 4/solution4.py
+++ 4/solution4.py
@@ -13,4 +13,50 @@ for line in lines:
         boards.append(board)
         board = []
     else:
-        board.append(list(filter(None, line.split(' '))))
+        board.append(list(map(int, list(filter(None, line.split(' '))))))
+boards.append(board)
+
+posWins = {}
+
+for x in range(len(boards)):
+    if x not in posWins.keys():
+        posWins[x] = []
+    for y in range(len(boards[x])):
+        posWins[x].append(boards[x][y])
+        column = []
+        for z in range(len(boards[x])):
+            column.append(boards[x][z][y])
+        posWins[x].append(column)
+
+drawn = []
+winBoard = -1
+winRow = []
+winNum = -1
+
+for c in calls:
+    drawn.append(int(c))
+    for k in posWins.keys():
+        for x in posWins[k]:
+            if set(x).issubset(set(drawn)):
+                winRow = x
+                winBoard = k
+                winNum = int(c)
+                break
+        if(winBoard > -1):
+            break
+    if(winBoard > -1):
+        break
+
+print(drawn)
+print(winNum)
+print(winRow)
+print(boards[winBoard])
+
+
+total = 0
+for x in range(len(boards[winBoard])):
+    for y in boards[winBoard][x]:
+        if y not in drawn:
+            total += y
+
+print(total * winNum)
